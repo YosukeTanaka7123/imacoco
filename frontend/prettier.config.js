ddf32e01ed5fac8f58da88cf93bc6c5f10112514
YosukeTanaka7123 <ikkik.skyscraper@gmail.com>
2023-06-26 23:54:02 +0900

    初期登録

---
+++ frontend/prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+
+  // 未使用インポート自動削除のスキップ
+  // https://github.com/simonhaenisch/prettier-plugin-organize-imports
+  organizeImportsSkipDestructiveCodeActions: false,
+}
